13e7aeeca5d09ed6322a340414d19bf17a62480c
Juan SR <jfrancisco1717@gmail.com>
2021-03-24 02:42:08 -0300

    Get tweets from an user

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,17 +1,17 @@
 <div class="flex p-4 border-b border-grey-400">
     <div class="mr-2 flex-shrink-0">
         <img
-            src="https://i.pravatar.cc/60"
+            src="https://i.pravatar.cc/60?u={{ $tweet->user->email }}"
             alt=""
-            class="rounded-full mr-4"
+            class="rounded-full mr-2"
         >
     </div>
 
     <div>
-        <h5 class="font-bold mb-4">Pita</h5>
+        <h5 class="font-bold mb-2">{{ $tweet->user->name }}</h5>
 
         <p class="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illo amet neque reprehenderit quas animi minus facere praesentium fugit maiores at, ipsum provident fugiat ipsam delectus expedita corporis voluptatibus consequuntur?
+            {{ $tweet->body }}
         </p>
     </div>
 </div>
