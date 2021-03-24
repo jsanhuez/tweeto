113d1dc0fe864722624a4e0e13f8612ec0e06fca
Juan SR <jfrancisco1717@gmail.com>
2021-03-24 01:37:36 -0300

    Add 'publish-tweet-panel' and 'tweet' views

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,17 @@
+<div class="flex p-4 border-b border-grey-400">
+    <div class="mr-2 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/60"
+            alt=""
+            class="rounded-full mr-4"
+        >
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">Pita</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est illo amet neque reprehenderit quas animi minus facere praesentium fugit maiores at, ipsum provident fugiat ipsam delectus expedita corporis voluptatibus consequuntur?
+        </p>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lg:flex">
-        <div class="lg:w-1/6">
+    <div class="lg:flex lg:justify-between">
+        <div class="lg:w-32">
             @include('_sidebar-links')
         </div>
 
-        <div class="lg:flex-1 lg:mx-10">
-            <div class="border border-blue-400 rounded-lg px-8 py-6">
-                <form action="">
-                    <textarea
-                        name="body"
-                        class="w-full"
-                        placeholder="What's up?"
-                    ></textarea>
+        <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+            @include('_publish-tweet-panel')
 
-                    <hr class="my-4">
-
-                    <footer class="flex justify-between">
-                        <img
-                            src="https://i.pravatar.cc/40"
-                            alt=""
-                            class="rounded-full mr-4"
-                        >
-
-                        <button
-                            type="submit"
-                            class="bg-blue-500 rounded-lg shadow py-2 px-3 text-white"
-                        >Tweetoo</button>
-                    </footer>
-                </form>
-            </div>
-
-            <div>
-
+            <div class="border border-gray-300 rounded-lg">
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
+                @include('_tweet')
             </div>
         </div>
 
-        <div class="lg:w-1/6">
+        <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
             @include('_friends-list')
         </div>
     </div>
