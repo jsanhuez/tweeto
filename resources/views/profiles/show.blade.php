bf478e9e9ff66daa348e43e0369c071f5717261b
Juan SR <jfrancisco1717@gmail.com>
2021-03-25 23:59:25 -0300

    Add follow/unfollow

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,27 +1,33 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app>
     <header class="mb-6 relative">
-        <img
-            src="/images/default-profile-banner.jpg"
-            alt="Profile banner"
-            class="mb-2"
-        >
+        <div class="relative">
+            <img
+                src="/images/default-profile-banner.jpg"
+                alt="Profile banner"
+                class="mb-2"
+            >
 
-        <div class="flex justify-between items-center mb-4">
+            <img
+                src="{{ $user->avatar }}"
+                alt=""
+                class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+                style="left: 50%"
+                width="150"
+            >    
+        </div>
+
+        <div class="flex justify-between items-center mb-6">
             <div>
                 <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
                 <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
             </div>
 
-            <div>
+            <div class="flex">
                 <a href="" class="rounded-full border border-grey-300 py-2 px-4 text-black text-xs mr-2">
                     Edit Profile
                 </a>
 
-                <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
-                    Follow Me
-                </a>
+                <x-follow-button :user="$user"></x-follow-button>
             </div>
         </div>
 
@@ -30,16 +36,9 @@
             Cupiditate quidem ex nemo rem quos officia aliquam odio deserunt totam dolor.
             Architecto ea sunt unde, beatae fugit laboriosam quisquam ipsum exercitationem.
         </p>
-
-        <img
-            src="{{ $user->avatar }}"
-            alt=""
-            class="rounded-full mr-2 absolute"
-            style="width: 150px; left: calc(50% - 75px); top: 138px"
-        >
     </header>
 
     @include('_timeline', [
         'tweets' => $user->tweets
     ])
-@endsection
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FollowsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
@@ -22,8 +23,10 @@ Route::get('/', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/tweets', [TweetController::class, 'index'])->name('home');
     Route::post('/tweets', [TweetController::class, 'store']);
+
+    Route::post('/profiles/{user:name}/follow', [FollowsController::class, 'store']);
 });
 
-Route::get('/profiles/{user}', [ProfileController::class, 'show'])->name('profile');
+Route::get('/profiles/{user:name}', [ProfileController::class, 'show'])->name('profile');
 
 Auth::routes();
